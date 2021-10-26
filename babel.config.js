fa1b0c4aed54932103e67d542857ff1fb66b6a8d
NullVoxPopuli <LPSego3+dev@gmail.com>
2021-10-25 21:16:34 -0400

    Keep removing dependencies until ember-cli-babel will be easier to debug

---
+++ babel.config.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const { buildEmberPlugins } = require('ember-cli-babel');
+
+module.exports = function (api) {
+  api.cache(true);
+
+  console.log('ugh'); // <-- I can see this in the terinal
+
+  // return {}; // <-- I only added this because nothing I changed below seemed to affect the build output.
+  // ^ I would expect this to break the build... or at the very least babel should complain about running in to decorators
+  //   and not having the decorator plugin loaded...
+
+  return {
+    presets: [
+      [
+        require.resolve('@babel/preset-env'),
+        {
+          modules: 'auto',
+          targets: {
+            // esmodules: true,
+            browsers: ['last 1 firefox versions'],
+          },
+        },
+      ],
+    ],
+    plugins: [
+      // [require.resolve('@babel/plugin-proposal-decorators'), { legacy: true }],
+      ...buildEmberPlugins(__dirname),
+    ],
+  };
+};
